55ba98c85e976951458df178575a306caf1cd644
Saito Tsutomu(KKE) <tsutomu@kke.co.jp>
2017-02-06 15:26:00 +0900

    a

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Make Semiar Text.
 3) Edit config.yml
 4) docker run -it --rm -v $PWD:/tmp tsutomu7/seminar-text
 5) Use out.pdf
+
+## config.yml
+
+Item|Description
+:--|:--
+file|Input file name
+range|List of target page number(0-)
+numup|Input pages per an output page
+hasTitle|Contents has a title page
+titles|List of string in a title page
+toEven|Make to even
+flowToDown|Page flow direction when 4 up
--- make.py
+++ make.py
@@ -7,87 +7,94 @@ from pdfrw import PdfReader
 from pdfrw.buildxobj import pagexobj
 from pdfrw.toreportlab import makerl
 from more_itertools import chunked
-try:
-    pdfmetrics.registerFont(TTFont('IPAexGothic', '/usr/share/fonts/ipaexg.ttf'))
-except:
-    pdfmetrics.registerFont(TTFont('IPAexGothic', 'C:/Windows/Fonts/ipaexg.ttf'))
-
-def addpage(np=[0]):
+def addHeaderFooter(np=[0]):
     np[0] += 1
-    c.setFillColor('gray')
-    c.drawRightString(w_-8, h_-16, header)
-    c.setFillColor('black')
-    c.drawCentredString(w_/2, mgn/2+2, '%d/%d'%(np[0],nn))
+    if header:
+        c.setFillColor('gray')
+        c.drawRightString(w_-8, h_-16, header)
+    if footer:
+        c.setFillColor('black')
+        c.drawCentredString(w_/2, mgn/2+2, footer%(np[0],len(cnts)))
+def addPage(c, w, h, numup, pg):
+    pgw, pgh = pg.w, pg.h
+    islandscape = pgw >= pgh
+    if not islandscape:
+        pgw, pgh = pgh, pgw
+    r = [min(w/pgh,h/pgw), min(w/pgw,h/2/pgh), min(w/pgh,h/pgw)/2][abs(numup)//2]
+    c.saveState()
+    if numup == 1:
+        if islandscape:
+            c.translate(pgh*r+(w-pgh*r)/2, (h-pgw*r)/2)
+            c.rotate(90)
+    elif numup == 2:
+        if islandscape:
+            c.translate((w-pgw*r)/2, (1-i%2)*pgh*r+(h-pgh*r*2)/2)
+        else:
+            c.translate((w*3-pgw*r)/2, (1-i%2)*pgh*r+(h-pgh*r*2)/2)
+            c.rotate(90)
+    elif numup == 4:
+        if islandscape:
+            c.translate((1+i//2)*w/2+(w/2-pgh*r)/2, (i%2)*h/2+(h/2-pgw*r)/2)
+            c.rotate(90)
+        else:
+            c.translate((i%2)*w/2+(w/2-pgh*r)/2, (1-i//2)*h/2+(h/2-pgw*r)/2)
+    elif numup == -4:
+        if islandscape:
+            c.translate((1+i%2)*w/2+(w/2-pgh*r)/2, (i//2)*h/2+(h/2-pgw*r)/2)
+            c.rotate(90)
+        else:
+            c.translate((i//2)*w/2+(w/2-pgh*r)/2, (1-i%2)*h/2+(h/2-pgw*r)/2)
+    c.scale(r, r)
+    c.doForm(makerl(c, pg))
+    c.setStrokeColor('gray')
+    c.rect(0,0,pg.w,pg.h)
+    c.restoreState()
 
 with open('config.yml', encoding='utf8') as fp:
     config = yaml.load(fp)
-header = config['param']['header']
-mgn = config['param']['margin']
-titpar = config['param']['titlepar']
 contents = config['contents']
-keys = ['title1','title2','title3','name','depart']
-c = Canvas(config['param']['outfile'], initialFontName='IPAexGothic', initialFontSize=12)
+param = config['param']
+mgn = param.get('margin', 10)
+fontFile = param.get('fontFile', '/usr/share/fonts/ipaexg.ttf')
+pdfmetrics.registerFont(TTFont('IPAexGothic', fontFile))
+fntsz = param.get('fontSize', 12)
+header = param.get('header')
+footer = param.get('footer')
+ttfntpos = param.get('titleFontPos', [])
+c = Canvas(param.get('outFile', 'out.pdf'),
+    initialFontName='IPAexGothic', initialFontSize=fntsz)
 w_, h_ = c._pagesize
 r_ = min(1 - 2*mgn/w_, 1 - 2*mgn/h_)
-w, h, nn = r_*w_, r_*h_, 0
+w, h = r_*w_, r_*h_
+cnts = []
 for cnt in contents:
-    numup = cnt['numup']
+    numup = cnt.get('numup', 4)
     assert numup in [1,2,4]
-    if cnt['hasTitle']:
-        nn += 2
+    toEven = cnt.get('toEven', False)
+    if cnt.get('hasTitle', False):
+        cnts.append((-1, cnt['titles'])) # header
+        if toEven:
+            cnts.append((0, None)) # blank
     pr = PdfReader(cnt['file'])
-    if isinstance(cnt['range'], str):
-        cnt['range'] = eval(cnt['range'])
-    if cnt['toEven']:
-        nn += (len(cnt['range']) + numup*2-1) // (numup*2)*2
-    else:
-        nn += (len(cnt['range']) + numup-1) // (numup)
-for cnt in contents:
-    numup = cnt['numup']
-    if cnt['hasTitle']:
-        addpage()
+    rng = cnt.get('range', 'range(pr.numPages)')
+    if isinstance(rng, str):
+        rng = eval(rng)
+    nm = numup if numup != 4 or not cnt.get('flowToDown', False) else -4
+    pages = [pagexobj(pr.pages[i]) for i in rng]
+    for pgs in chunked(pages, numup):
+        cnts.append((nm, pgs))
+    if toEven and (len(pages)+numup-1)//numup % 2:
+        cnts.append((0, None)) # blank
+for numup, pgs in cnts:
+    addHeaderFooter()
+    if numup == -1: # header
         c.translate(80, h/2-80)
-        for p,s in zip(titpar, [cnt[k] for k in keys]):
+        for p,s in zip(ttfntpos, pgs):
             c.setFontSize(p[0])
             c.drawString(*p[1:], s)
-        c.showPage()
-        addpage()
-        c.showPage()
-    pr = PdfReader(cnt['file'])
-    pages = [pagexobj(pr.pages[i]) for i in cnt['range']]
-    for pgs in chunked(pages, numup):
-        addpage()
+    elif numup != 0:
         c.translate(w_/2-w/2, h_/2-h/2)
         for i, pg in enumerate(pgs):
-            pgw, pgh = pg.w, pg.h
-            islandscape = pgw >= pgh
-            if not islandscape:
-                pgw, pgh = pgh, pgw
-            r = [min(w/pgh,h/pgw), min(w/pgw,h/2/pgh), min(w/pgh,h/pgw)/2][numup//2]
-            c.saveState()
-            if numup == 1:
-                if islandscape:
-                    c.translate(pgh*r+(w-pgh*r)/2, (h-pgw*r)/2)
-                    c.rotate(90)
-            elif numup == 2:
-                if islandscape:
-                    c.translate((w-pgw*r)/2, (1-i%2)*pgh*r+(h-pgh*r*2)/2)
-                else:
-                    c.translate((w*3-pgw*r)/2, (1-i%2)*pgh*r+(h-pgh*r*2)/2)
-                    c.rotate(90)
-            elif numup == 4:
-                if islandscape:
-                    c.translate((1+i//2)*w/2+(w/2-pgh*r)/2, (i%2)*h/2+(h/2-pgw*r)/2)
-                    c.rotate(90)
-                else:
-                    c.translate((i%2)*w/2+(w/2-pgh*r)/2, (1-i//2)*h/2+(h/2-pgw*r)/2)
-            c.scale(r, r)
-            c.doForm(makerl(c, pg))
-            c.setStrokeColor('gray')
-            c.rect(0,0,pg.w,pg.h)
-            c.restoreState()
-        c.showPage()
-    if cnt['toEven'] and (len(pages)+numup-1)//numup%2:
-        addpage()
-        c.showPage()
+            addPage(c, w, h, numup, pg)
+    c.showPage()
 c.save()
